9e7cf98953089ede6092ba966030d047b0586b0d
simon <simon@sig.tw>
2022-05-03 18:14:30 +0800

    update

--- sig-wc-show-coupon.php
+++ sig-wc-show-coupon.php
@@ -22,7 +22,6 @@ if ( ! class_exists( 'Sig_Wc_Show_Coupon' ) ) :
         public function __construct() {
 
             add_action( 'wc_ajax_update_my_coupon', array( __CLASS__, 'ajax_update_my_coupon' ) );
-            add_action( 'wc_ajax_use_my_coupon', array( __CLASS__, 'ajax_use_my_coupon' ) );
 
             add_action( 'wp_footer', array( __CLASS__, 'footer_html' ) );
 
@@ -35,16 +34,21 @@ if ( ! class_exists( 'Sig_Wc_Show_Coupon' ) ) :
         ?>
             <style type="text/css">
             #coupons_list{ float: left;width: 48%; }
-            #coupons_list .item_coupon{ width: 45%; padding:10px; margin: 0 5px 15px; border: 2px dashed #bbbbbb; display:flex;flex-direction: column; justify-content: space-between;  min-height: 130px;}
-            #coupons_list .item_coupon.active{ background-color: #00dd88; cursor: pointer; border-color: #555555 }
-            #coupons_list .item_coupon.used{ background-color: #eeeeee; border-color: #555555 }
-            #coupons_list .item_coupon strong{ font-size: 26px; text-align: center;}
+            #coupons_list .item_coupon{ box-sizing: border-box; position: relative; display: flex; justify-content: space-between; align-items: center; height: 120px; margin-bottom: 20px; box-shadow:1px 1px 4px  rgba(0,0,0,0.5); -moz-box-shadow:1px 1px 4px  rgba(0,0,0,0.5); -webkit-box-shadow:1px 1px 4px  rgba(0,0,0,0.5);}
+            #coupons_list .item_coupon.active{ background: linear-gradient(to right, #ffb300 0%, #ffb300 35%, #eeeeee 35%, #eeeeee 100%); }
+            #coupons_list .item_coupon.used{ background: linear-gradient(to right, #ffb300 0%, #ffb300 35%, #eeeeee 35%, #eeeeee 100%); filter: grayscale(0.6); }
+            #coupons_list .item_coupon.disabled{ background-color: #f2f2f2; }
+            #coupons_list .item_coupon:before{ content: ""; position: absolute; display: block; top: 0; left: calc( 35% - 3px ); width: 3px; height: 100%; border-left: 3px dashed #fff; }
+            #coupons_list .item_coupon .code{ box-sizing: border-box; width: 35%; text-align: center; font-size: 26px; font-weight: 600; }
+            #coupons_list .item_coupon .note{ box-sizing: border-box; width: 65%; height: 100%; padding: 15px; display: flex; justify-content: flex-start; flex-direction: column; }
+            #coupons_list .item_coupon .btn_coupon{ font-size: 14px; font-weight: 400; text-align: center; border: 1px solid #ffb300; color: #ffb300; padding: 2px 10px; margin-left: auto; margin-top: auto; text-decoration: none; }
+            #coupons_list .item_coupon .btn_coupon:hover, #coupons_list .item_coupon .btn_coupon:focus{ background-color: #ffb300; color: #ffffff; }
+            #coupons_list .item_coupon .btn_coupon[disabled], #coupons_list .item_coupon .btn_coupon[disabled]:hover{ border:1px solid #bbbbbb; color: #bbbbbb; background-color: transparent;}
             </style>
             <script>
-
             jQuery( function( $ ) {
 
-                $( 'body' ).on( 'updated_cart_totals updated_shipping_method', function(e){
+                $( 'body' ).on( 'updated_cart_totals', function(e){
                     //code
                     console.log('=='+e.type);
                     $.get( "<?php echo WC_Ajax::get_endpoint( 'update_my_coupon' ) ?>", function( data ) {
@@ -52,28 +56,11 @@ if ( ! class_exists( 'Sig_Wc_Show_Coupon' ) ) :
                     });
                 });
 
-
-                $('#coupons_list').on('click','.active',function(){
-                    var code = $(this).data('code');
+                $('#coupons_list').on('click','button[name=user_coupon]',function(){
+                    var code = $(this).val();
                     if(code){
-
-                        $.post( "<?php echo WC_Ajax::get_endpoint( 'use_my_coupon' ) ?>", { code: code } )
-                        .done(function( data ) {
-                            console.log('coupon...');
-                            if( data == 'ok' ) {
-                                //$("[name='update_cart']").trigger("click");
-                                //$( 'body' ).trigger( 'applied_coupon', [ code ] );
-                                //$( 'body' ).trigger( 'updated_cart_totals' );
-
-                                //$( document.body ).trigger( 'updated_wc_div' );
-
-                                //$( document.body ).trigger( 'updated_shipping_method' );
-                                //jQuery('refresh_cart_fragment');
-
-                            }
-
-                        });
-
+                        $('#coupon_code').val(code);
+                        $('[name=apply_coupon]').trigger('click');
                     }
                 });
             });
@@ -95,7 +82,7 @@ if ( ! class_exists( 'Sig_Wc_Show_Coupon' ) ) :
         public static function ajax_update_my_coupon(){
 
             echo '<h2>'.__('Coupon','woocommerce').'</h2>';
-            echo '<div style="display:flex;flex-wrap: wrap;">';
+            echo '<div style="">';
 
             global $wpdb;
             global $woocommerce;
@@ -121,18 +108,29 @@ if ( ! class_exists( 'Sig_Wc_Show_Coupon' ) ) :
 
                 $class_name = [];
                 $class_name[] = 'item_coupon';
+                $btn_name = '';
 
                 if( in_array( strtolower($code->post_title), $woocommerce->cart->applied_coupons) ) {  //使用中
                     $class_name[] =  'used';
+                    $btn_name = '<a href="https://woo.abc.com/cart/?remove_coupon='. strtolower($code->post_title) .'" class="btn_coupon woocommerce-remove-coupon" data-coupon="'. strtolower($code->post_title) .'">移除折價券</a>';
+                }else if( $c->is_valid() ) {
+                    $class_name[] = 'active';  //可使用
+                    $btn_name = '<button class="btn_coupon" type="button" name="user_coupon" value="'.$code->post_title.'">使用折價券</button>';
                 }else{
-                    if( $c->is_valid() ) $class_name[] = 'active';  //可使用
+                    $class_name[] = 'disabled';
+                    $btn_name = '<button class="btn_coupon" type="button" disabled>未符合資格</button>';
                 }
 
         ?>
-                <div class="<?php echo implode(" ",$class_name)?>" data-code="<?php echo strtolower($code->post_title)?>">
-                    <small><i><?php echo $note;?></i></small>
-                    <strong><?php echo $code->post_title?></strong>
-                    <small>使用期限：<?php echo (empty($c->get_date_expires()) ? '無': $c->get_date_expires()->date( 'Y-m-d' ).' 前')  ?></small>
+                <div class="<?php echo implode(" ",$class_name)?>" data-code="<?php echo $code->post_title?>">
+                    <div class="code">
+                        <?php echo $code->post_title?></strong>
+                    </div>
+                    <div class="note">
+                        <div><?php echo $note;?></div>
+                        <small>使用期限：<?php echo (empty($c->get_date_expires()) ? '無': $c->get_date_expires()->date( 'Y-m-d' ).' 前')  ?></small>
+                        <?php if( !empty($btn_name)) echo $btn_name ?>
+                    </div>
                 </div>
         <?php
 
@@ -141,26 +139,6 @@ if ( ! class_exists( 'Sig_Wc_Show_Coupon' ) ) :
             echo '</div>';
 
         }
-
-        public static function ajax_use_my_coupon(){
-
-            global $woocommerce;
-
-            if( !empty($_POST['code']) ){
-                $coupon_code = sanitize_text_field( wp_unslash( $_POST['code'] ) );
-
-                if( WC()->cart->add_discount( sanitize_text_field( $coupon_code ) ) ){
-                    echo 'ok';
-                    //WC_Ajax::get_refreshed_fragments();
-                }else{
-                    echo 'not ok';
-                }
-            }
-
-            //wp_send_json($data)
-            wp_die();
-        }
-
 
     }
 
